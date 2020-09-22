777e05e7fb49600e3d800cc306296d2b00ccb1d0
Rigoberto <mario_dzi@hotmail.com.ar>
2020-09-22 01:39:46 -0300

    actualizacion

--- vista/estructura/menu.php
+++ vista/estructura/menu.php
@@ -1,7 +1,9 @@
 <!doctype html>
 <html lang="es">
 <head>
-
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="../../TP2/vista/CSS/4.5.2/bootstrap.min.css" >
@@ -11,34 +13,29 @@
     <title><?php $Titulo?></title>
 </head>
 
-
+<body>
 
 
 
 
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
 
-    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#"></a>
-   
+
 
 </nav>
-<div class="container " style="height: 400px; width: 15%; border: 1px solid BLACK; border-radius: 2px; float: 
+<div class="container fluid" style=" width: 16%; float: 
 left;">
-
-<div class="container" name="menu" style="color:blue;">Este es el Menu.</div>
-<body>
-
 
 
     <div class="row">
     
-        <nav id="sidebarMenu" >
+        <nav id="sidebarMenu" class=" col-lg-2 d-md-block sidebar collapse" >
 
 
             <div class="sidebar-sticky pt-3">
 
 
-                <ul>
+            <ul >
                     <li class="nav-item">
                         <a class="nav-link active" href="../ejercicio-v2/amarchivo.php">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
@@ -82,3 +79,5 @@ left;">
         </body>
     </div>
 </div>
+
+
